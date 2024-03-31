b4553bc9242765e72b22272fd3d626e60c654e21
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-03-31 13:22:59 +0500

    elif fayli qo'shildi

---
+++ elif.py
@@ -0,0 +1,80 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 17:37:38 2024
+11-dars: if-elif-else
+
+@author: ПИРМУХАММАД
+"""
+
+
+#yosh = int(input("yosh"))
+#if yosh<=4:
+#    narx=0
+#elif yosh<=12:
+#    narx=5000
+#elif yosh<= 18:
+#    narx=1000
+#else:
+#    narx=15000
+#print(f'kirish {narx} som')
+
+#kun =input("what is day today?")
+#harorat = float(input("when is temprich?"))
+#if kun.lower() =='yakshanba'  and harorat>36.6:
+#    print("chomiilgani kettik")
+#elif kun.lower() =='yakshanba' and harorat<36.6:
+#    print("uyda o'tiramiz")
+#else:
+#    print("bugun ish kuni")
+
+
+#kun =input("what is day today?")
+#harorat = float(input("when is temprich?"))
+#if (kun.lower() =='yakshanba' or kun.lower() =='shanba') and harorat>36.6:
+#    print("chomiilgani kettik")
+#elif (kun.lower() =='yakshanba' or kun.lower() =='shanba') and harorat<36.6:
+#    print("uyda o'tiramiz")
+#else:
+#    print("bunaqa kun yo'q")
+  
+
+#narx =15000
+#choy = True
+#non =True
+#salat = False
+#if choy and non and salat :
+#    narx=narx+10000
+#elif choy or non or salat:
+#    narx=narx+5000
+#print(f'jami{narx}')
+
+
+#narx=15000
+#choy = True
+#non =True
+#salat = False
+#osh=True
+#suv= False
+
+#if choy:
+#    narx=narx+1000
+#if osh:
+#    narx=narx+20000
+#print(f'jami{narx}')
+
+#menu=['osh','manti','kabop']
+#buyurtma=[]
+#if buyurtma:
+#    for taom in buyurtma:
+#        if taom in menu:
+#          print(f'bizda {taom} bor')
+#        else:
+#          print(f'bizda bu {taom} yoq')
+#else:
+#    print('savatingiz bosh')
+
+
+
+
+    
