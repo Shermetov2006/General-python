593b8cd198217171c5d474d21535f9303f8812de
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-01 13:19:28 +0500

    o'zgarish

--- Lug'at/lugat.py
+++ Lug'at/lugat.py
@@ -1,12 +1,22 @@
-#otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga
-#shu inson haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, sha
-#hri, manzili va hokazo). Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring
-#:Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+#otam={"ism":"qudrat","tyil":1981,"viloyat":"xorazm"}
+#tyil=otam["tyil"]
+#vil=otam["viloyat"]
+#print(f"Otamning ismi {otam["ism"].upper()},{tyil}-yil,{vil.upper()}\
+# viloyatida tug'ilgan.")
 
 
-otam={"ism":"qudrat","tyil":1981,"viloyat":"xorazm"}
-tyil=otam["tyil"]
-vil=otam["viloyat"]
-print(f"Otamning ismi {otam["ism"].upper()},{tyil}-yil,{vil.upper()}\
- viloyatida tug'ilgan.")
+
+
+#taomlar = {
+#    "ali": "osh",
+#    "vali": "shashlik",
+#    "hasan": "lag'mon",
+##    "husan": "mastava",
+ #   "olim": "somsa",
+#}
+
+#taom = taomlar["ali"]
+#print(f"Alining sevimli taomi {taom}")
+
+ 
 
