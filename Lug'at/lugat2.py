3a96500c44b66fc9f56fbcbf76d08b344cb858ad
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-05 12:58:52 +0500

    yangi

---
+++ Lug'at/lugat2.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 12:21:46 2024
+
+@author: pirmuhammad
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#car0 = {
+#        'model':'lacetti',
+#        'rang':'oq',
+#        'yil':2018,
+#        'narh':13000,
+#        'km':50000,
+#        'korobka':'avtomat'
+#        }#
+
+#car1 = {
+#        'model':'nexia 3',
+#        'rang':'qora',
+#       'yil':2015,
+#        'narh':9000,
+#        'km':89000,
+#        'korobka':'mexanika'
+#        }
+
+#car2 = {
+#        'model':'gentra',
+#        'rang':'qizil',
+#       'yil':2019,
+#        'narh':15000,
+#       'km':20000,
+#        'korobka':'mexanika'
+#        }
+
+#cars = [car0, car1, car2]
+#for car in cars:
+#    print(f"{car['model'].title()}, "
+#          f"{cars[2]['rang'].title()} "
+#          f"{cars[2]['model']}")
+
+
+
+#malubus=[]
+#for malibu in range(10):
+#    car={ 'model':'malibu',
+#         'rang':None,
+#         'yil':'2024',
+ #        'narx':None,
+  #       'km':0,
+#         'korobka': 'avtomat'
+#    
+    
+#for malibu in malubus[:3]:
+#    malibu ['rang']= 'qizil'
+    
+    
+#for malibu in malubus[3:6]:
+ #   malibu['rang']='qora'
+    
+#for malibu in malubus[6:8]:
+#    malibu['rang']='shikalat svet'
+    
+#for malibu in malubus[8:]:
+#    malibu['rang']='oq'
+#    malibu['korobka']='mexanik'
+    
+#for malibu in malubus:
+#    if malibu ['korobka'] == 'avtomat':
+ #       malibu['narx']=40000
+#    else:
+#        malibu['narx']=35000
+    
+
+
