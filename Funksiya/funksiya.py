713ca7fc5996e408206178aae19e79c474b832a7
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-09 10:05:18 +0500

    new

---
+++ Funksiya/funksiya.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 09:55:31 2024
+
+@author: pirmuhammad
+"""
+
+def salom_ber():
+    print("assalomu aleykum")
+salom_ber()
+
+def salom__ber(ism):
+    print(f"assalomu aleykum {ism}")
+salom__ber("olim")
+salom__ber("usmon")
