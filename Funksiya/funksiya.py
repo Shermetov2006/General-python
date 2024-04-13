cefca5bc219a7055ce2fb8a7c1d8d321ee1036b1
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-13 09:44:16 +0500

    new

--- Funksiya/funksiya.py
+++ Funksiya/funksiya.py
@@ -33,3 +33,53 @@ Created on Tue Apr  9 09:55:31 2024
 #    print(f"{hozirgi_vaqt-tugilgan_yil}")
 #yosh_hisoblash(2005)
 
+# -*- coding: utf-8 -*-
+
+
+#3)#Foydalanuvchidan son olib, son juft yoki toqligini konsolga
+# vchiqaruvchi funksiya yozing.
+
+#def j_or_t(son):
+#    if (son)%2!=0:
+#        print(f"{son}  toq")
+#    else:
+#        print(f"{son} juft")
+        
+#j_or_t(5)
+#j_or_t(10)
+
+
+
+#4)#Foydalanuvchidan ikkita son olib, ulardan kattasini konsolga
+# chiqaruvchifunksiya yozing. Agarsonlar teng bo'lsa "Sonlarteng"deganxabarnichiqaring.
+
+#def katta_kichik(son,son2):
+#    if son>son2:
+#        print(f"{son}>{son2}")
+#    elif son==son2:
+#        print(f"{son}={son2}")
+#    else:
+#        print(f"{son}<{son2}")
+#        
+#katta_kichik(5,5 )
+
+
+#5)#Foydalanuvchidan x va y sonlarini olib, ni konsolga
+# chiqaruvchi funksiya yozing.
+#Yuqoridagi funksiyada y uchun 2 standart qiymatini bering.
+
+#def son(x,y=2):
+#    print(x,y)
+#son(5)
+ 
+
+#6)#Foydalanuvchidan son qabul qilib, sonni 2 dan 10 gacha bo'lgan
+# sonlarga qoldiqsiz bolinishini tekshiruvchi funksiya yozing. Natijalarni
+# konsolga chiqaring.  
+
+#def devide(son):
+#    for i in range(2,11):
+#        if not son%i:
+#            print(f"{son} {i}ga qoldiqsiz bo'linadi")
+#devide(8)
+
---
+++ Funksiya/homework.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 11:57:12 2024
+
+@author: PITMUHAMMAD
+"""
+#def tyil(ism,yosh,hozirgi_yil=2024):
+#    """Foydalanuvchi ismi va yoshini so'rab, 
+#    uning tug'ilgan yilini hisoblaydigan funksiya yozing."""
+#    print(f'{ism} {hozirgi_yil-yosh}-yil')
+#tyil("javohir",17)
+
+## Foydalanuvchidan son olib, uning kvadrati va kubini konsolga chiqaruvchi funksiya yozing.
+#def kv_kub(son):
+#    """Kiritilgan sonning kvadrati va kubini konsolga chiqaruvchi funksiya"""
+#    print(f"{son} ning kvadrati {son**2} ga, kubi {son**3} ga teng")
+#kv_kub(-4)
+
