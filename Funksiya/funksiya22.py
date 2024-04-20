3f6e096be733af01fd9fda5899771a7afee6f043
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-20 10:52:48 +0500

    davomi bor

---
+++ Funksiya/funksiya22.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 10:16:49 2024
+
+@author: фвьшт
+"""
+
+# def summa(*sonlar):
+#     yigindi=0
+#     for son in sonlar:
+#         yigindi+=son
+#     return yigindi
+# print(sum(range(1,101)))
