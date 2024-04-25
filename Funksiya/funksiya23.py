caa0a52feff777670b7f860be6bc8883cbce49c2
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-25 13:13:32 +0500

    ooyin

--- Funksiya/funksiya23.py
+++ Funksiya/funksiya23.py
@@ -6,7 +6,8 @@ Created on Tue Apr 23 12:11:14 2024
 """
 
 import random as r 
-savol="\n(DASTURIMIZ O'ZBEK TILIDA')\n"
+savol="\n(DASTURIMIZ O'ZBEK TILIDA)\n"
+savol+="(HAR BIR MATNNI DIQQAT BILAN O'QING ILTIMOS!)\n"
 savol += "(dasturni to'xtatish uchun 'exit' deb yozing yoki ENTER): "
 
 while True:
@@ -17,30 +18,139 @@ while True:
         
     
     else:
-        m=(input("\nBIZNING O'YINDA QATNASHASIZMI:\n ha\yoq: "))
+        m=(input("\nO'YIN NARXI 5000 SO'M! \nBIZNING O'YINDA QATNASHASIZMI:\n ha\yoq: "))
 
         if m.lower()=="ha" :
             
-            c=int(input("O'yin 5000 so'm \n pul kiriting: "))
-            if c>=5000:
-                print("Rahmat. O'YINNI BOSHLASHINGIZ MUMKIN! ")
+            
+            
+            while True:
+                c=float(input("\nBIZNING O'YINIMIZDA SIZ QANDAYDIR SON TANLAYSIZ RANDOM HAM QANDAYDIR SON TANLAYDI "
+                       "\nAGAR IKKALA SON YOKI RAQAM TENG BO'LSA SIZ YUTASIZ. AKS HOLDA YUTQAZASIZ!\n" 
+                         "\n SIZ 5000 SO'MDAN QANCH KO'PROQ PUL KIRITSANGIZ, BIZ O'SHA PULNI BOLALAR UYIGA BERAMIZ.\n \nO'YIN NARXI 5000 so'm \n pul kiriting: "))
+                
+                if c==5000:
+                    print("\nRahmat. O'YINNI BOSHLASHINGIZ MUMKIN! ")
+                    break
+                    
+                    b= int(input("son kiriting: "))
+                    a=r.randint(1,2)
+                    if a==b:
+                        print("\nSIZ YUTDINGIZ. [YOU WIN] \nSOVG'ANGIZNI BIZNING FILIALLARDAN OLIB KETISHINGIZ MUMKIN."
+                            f"RANDOM TANLAGAN RAQAM {a}")
+                    
+                                    
+                elif c>5000:
+                    print(f"\nRAHMAT. SIZ {c-5000} so'm ko'p pul berdingiz. BIZ BU PULNI SIZNING NOMINGIZDAN BOLALAR UYIGA BERAMIZ."
+                          f"\nSIZ UYALMASDAN {c-5000} SO'M BERDIM DEB HIJOLAT BO'LMANG. SAVOBNING KATTA KICHIKI BO'LMAYDI.")
+                    break
                 
                 b= int(input("son kiriting: "))
-                a=r.randint(1, 5)
+                a=r.randint(1,2)
                 if a==b:
-                    print("SIZ YUTDINGIZ. [YOU WIN] \nSOVG'ANGIZNI BIZNING FILIALLARDAN OLIB KETISHINGIZ MUMKIN.")
+                    print("\nSIZ YUTDINGIZ. [YOU WIN] \nSOVG'ANGIZNI BIZNING FILIALLARDAN OLIB KETISHINGIZ MUMKIN."
+                            f"RANDOM TANLAGAN RAQAM {a}")
+                    
                 else:
-                    print(f"SIZ YUTQAZDINGIZ SIZ TALLAGAN RAQAM {b} \nTASODIFIY SON {a}")
-                
+                    print("PULINGIZ KAM!")
             
+            
+            
+            
+            
+            
+            
+            # c=float(input("\nBIZNING O'YINIMIZDA SIZ QANDAYDIR SON TANLAYSIZ RANDOM HAM QANDAYDIR SON TANLAYDI "
+            #             "\nAGAR IKKALA SON YOKI RAQAM TENG BO'LSA SIZ YUTASIZ. AKS HOLDA YUTQAZASIZ!\n" 
+            #            "\n SIZ 5000 SO'MDAN QANCH KO'PROQ PUL KIRITSANGIZ, BIZ O'SHA PULNI BOLALAR UYIGA BERAMIZ.\n \nO'YIN NARXI 5000 so'm \n pul kiriting: "))
+            
+            # # while True:
+                # if c==5000:
+                #     print("\nRahmat. O'YINNI BOSHLASHINGIZ MUMKIN! ")
+                #     break
+                    
+                #     b= int(input("son kiriting: "))
+                #     a=r.randint(1,2)
+                #     if a==b:
+                #         print("\nSIZ YUTDINGIZ. [YOU WIN] \nSOVG'ANGIZNI BIZNING FILIALLARDAN OLIB KETISHINGIZ MUMKIN."
+                #             f"RANDOM TANLAGAN RAQAM {a}")
+                    
+            #     elif c>5000:
+                    # print(f"\nRAHMAT. SIZ {c-5000} so'm ko'p pul berdingiz. BIZ BU PULNI SIZNING NOMINGIZDAN BOLALAR UYIGA BERAMIZ."
+                    #       f"\nSIZ UYALMASDAN {c-5000} SO'M BERDIM DEB HIJOLAT BO'LMANG. SAVOBNING KATTA KICHIKI BO'LMAYDI.")
+                    # break
                 
-            else:
-                print("PULINGIZ KAM!")
+                    # b= int(input("son kiriting: "))
+                    # a=r.randint(1,2)
+                    # if a==b:
+                    #     print("\nSIZ YUTDINGIZ. [YOU WIN] \nSOVG'ANGIZNI BIZNING FILIALLARDAN OLIB KETISHINGIZ MUMKIN."
+                    #         f"RANDOM TANLAGAN RAQAM {a}")
+            #         else:
+            #             print(f"SIZ YUTQAZDINGIZ SIZ TALLAGAN RAQAM {b} \nTASODIFIY SON {a}")
+                    
+                
+            #     else:
+            #         print("PULINGIZ KAM!")
+
+        
+        
+                
+                
+                
+                
+                
 
 
 
         else:
             print("TASHRIFINGIZ UCHUN RAHMAT")
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            # while True:
+            #     a=int(input("pul kiriting: "))
+            #     if a==5000 :
+            #         print("RAHMAT!")
+            #         break
+
+            #     elif a>5000:
+            #         print(f"RAHMAT. SIZ {a-5000} so'm ko'p pul berdingiz. BIZ BU PULNI SIZNING NOMINGIZDAN BOLALAR UYIGA BERAMIZ."
+            #               f"\nSIZ UYALMASDAN {a-5000} SO'M BERDIM DEB HIJOLAT BO'LMANG. SAVOBNING KATTA KICHIKI BO'LMAYDI.")
+            #         break
+                    
+            #     else:
+            #         print("yo'q")
 
 
    
