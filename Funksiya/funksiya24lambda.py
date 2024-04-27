0e1d01d6319dd3ab39121de7508f2e31e8c0dc5c
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-04-27 12:56:28 +0500

    mnb

---
+++ Funksiya/funksiya24lambda.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 11:59:57 2024
+
+@author: фвьшт
+"""
+# import math
+
+# uzunlik= lambda pi, r : 2*pi*r
+# print(uzunlik(math.pi,10))
+
+# def daraja(n):
+#     return lambda x: x**n
+# kvadrat=daraja(2)
+# kub=daraja(3)
+
+# from math import sqrt
+# sonlar=list(range(1,10))
+# ildizlar=list(map(sqrt, sonlar))
+# print(ildizlar)
+
+# sonlar=list(range(1,10))
+# kvadratlar=list(map(lambda x: x*x, sonlar))
+# print(kvadratlar)
+
+
+# a=[4,5,6]
+# b=[7,8,9]
+# a_plus_b=list(map(lambda x,y: x+y, a,b))
+# print(a_plus_b)
+
+import random as  r
+
