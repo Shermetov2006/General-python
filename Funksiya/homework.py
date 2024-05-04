b1d300bd6cbae782069cc7382b42554571d92ad8
pirmuhammad <pirmuhammadshermetov405@gmail.com>
2024-05-04 10:57:54 +0500

    oop

--- Funksiya/homework.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 23 12:08:57 2024
-
-@author: фвьшт
-"""
-
---
+++ OOP/class2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 10:28:18 2024
+
+@author: фвьшт
+"""
+
+# class user:
+#     def __init__(self, ism , login , parol):
+#         self.ism =ism 
+#         self.login=login
+#         self.parol=parol
+#         self.bosqich=1
+        
+#     def get_ism(self):
+#         return self.ism
+#     def get_login(self):
+#         return self.login
+#     def get_parol(self):
+#         return self.parol
+#     def set_bosqich(self,yangi_bosqich):
+#         return self.yangi_bosqich
+#     def updata_bosqich(self):
+#         self.bosqich+=1
+      
+#     def get_info(self):
+#         return f"ismim {self.ism}  login:{self.login.upper()} parol:{self.parol} {self.bosqich}-bosqich talabsi"
+        
+        
+# users=user("maqsad", "Maq", 12345)
+# users2=user("vcx", "vcx", 45656) 
